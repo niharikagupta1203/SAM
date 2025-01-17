973331e0d46aaaa8c6e869a3e48801f1e10cc614
niharikagupta1203 <139216225+niharikagupta1203@users.noreply.github.com>
2025-01-17 21:03:15 +0530

    SAM

--- SAM.py
+++ SAM.py
@@ -5,15 +5,7 @@ if using_colab:
     print("PyTorch version:", torch.__version__)
     print("Torchvision version:", torchvision.__version__)
     print("CUDA is available:", torch.cuda.is_available())
-    import sys
-    !{sys.executable} -m pip install opencv-python matplotlib
-    !{sys.executable} -m pip install 'git+https://github.com/facebookresearch/segment-anything.git'
-    
-    !mkdir images
-    !wget -P images https://raw.githubusercontent.com/facebookresearch/segment-anything/main/notebooks/images/truck.jpg
-    !wget -P images https://raw.githubusercontent.com/facebookresearch/segment-anything/main/notebooks/images/groceries.jpg
-        
-    !wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
+import sys
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
